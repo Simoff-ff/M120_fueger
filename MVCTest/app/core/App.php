79f346f7056112e6aea66af88ef6e30de49cb6fc
Simoff-ff <62390925+Simoff-ff@users.noreply.github.com>
2021-03-29 07:43:51 +0200

    update

---
+++ MVCTest/app/core/App.php
@@ -0,0 +1,41 @@
+<?php
+    class App{
+
+        protected $controller = "home";
+        protected $method = "index";
+        protected $params = [];
+
+        public function __construct(){
+            $url = parseUrl();
+            if(file_exists('../app/controllers/'.$url[0].'.php')) {
+               require_once('../app/controllers/'.$url[0].'.php');
+               unset($url[0]);
+            }
+
+            require_once('../app/controllers/'.$url[0].'.php');
+            $this->controller = new $this->controller;
+
+            if(isset($url[1])) {
+                if(method_exists($this->controller, $url[1])) {
+                    $this->method = $url[1];
+                    unset($url[1]);                
+                }
+            }
+            $this->params = $url ? array_values($url) : [];
+            call_user_func_array([$this->controller, $this->method], $this->params);
+                
+            
+        }
+
+        public function parseUrl(){
+            if(isset($_GET["url"])){
+                return $url = explode("/", rtrim($_GET['url'], ’/’));
+            }
+        }
+
+        
+
+    }
+
+
+?>
