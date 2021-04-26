0fce4cdea61f2bd30f0b6980a337b77b29442a91
Simoff-ff <62390925+Simoff-ff@users.noreply.github.com>
2021-04-26 07:47:50 +0200

    rename

--- m120_proj/public/index.php
+++ m120_proj/public/index.php
@@ -23,7 +23,7 @@ require_once("../app/init.php");
     <meta name="generator" content="Jekyll v4.1.1" />
     <title>Album example · Bootstrap</title>
 
-    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
 
     <link
