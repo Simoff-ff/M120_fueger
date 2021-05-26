210f5e45f107426ce88eb514dd950dcd87f4e43c
21qPopp <62390730+21qPopp@users.noreply.github.com>
2021-05-26 20:24:17 +0200

    css -> html
    CSS ist fast fertig für die ersten Seiten.
    HTML der Startseite angepasst

--- m120_proj/app/views/home/Startseite.php
+++ m120_proj/app/views/home/Startseite.php
@@ -20,14 +20,12 @@ if ($speicher == 1) {
         <main>
             <form>
                 <div class="eingabeFelder">
-                    <label for="vonWo">Von wo startest du?
-                        <input type="text" id="vonWo" name="vonWo">
-                    </label>
-                    <label for="wohin">Wo ist dein Zeil?
-                        <input type="text" id="wohin" name="wohin">
-                    </label>
+                        <input type="text" id="vonWo" name="vonWo"  placeholder="Von wo startest du?">
+                        <input type="text" id="wohin" name="wohin" placeholder="Wo ist dein Ziel?">
                 </div>
-                <button type="submit"  name="speichern" value="1">Weiter</button>
+                <div class="buttonForm">
+                    <button type="submit"  name="speichern" value="1">Weiter</button>
+                </div>
             </form>
             <?php echo '<p>' . $vonWo . '</p>' ?>
             <?php echo '<p>' . $wohin . '</p>' ?>
