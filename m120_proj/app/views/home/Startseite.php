0182f754b1e2be7b65560425b82de8dde68277ce
21qPopp <62390730+21qPopp@users.noreply.github.com>
2021-05-26 20:28:00 +0200

    Update Startseite.php

--- m120_proj/app/views/home/Startseite.php
+++ m120_proj/app/views/home/Startseite.php
@@ -19,7 +19,6 @@ if ($speicher == 1) {
         </header>
         <main>
             <form>
-<<<<<<< HEAD
                 <div class="eingabeFelder">
                         <input type="text" id="vonWo" name="vonWo"  placeholder="Von wo startest du?">
                         <input type="text" id="wohin" name="wohin" placeholder="Wo ist dein Ziel?">
@@ -27,15 +26,6 @@ if ($speicher == 1) {
                 <div class="buttonForm">
                     <button type="submit"  name="speichern" value="1">Weiter</button>
                 </div>
-=======
-                <label for="vonWo">Von wo startest du?
-                    <input type="text" id="vonWo" name="vonWo">
-                </label>
-                <label for="wohin">Wo ist dein Ziel?
-                    <input type="text" id="wohin" name="wohin">
-                </label>
-                <button type="submit"  name="speichern" value="1">Weiter</button>
->>>>>>> 24ec8ca58f3700c30867b693f45b0577f1510050
             </form>
             <?php echo '<p>' . $vonWo . '</p>' ?>
             <?php echo '<p>' . $wohin . '</p>' ?>
